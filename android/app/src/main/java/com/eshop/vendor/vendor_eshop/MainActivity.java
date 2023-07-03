c5d3632c5932a19a65b31a8e875bd260e0d7816e
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-07-03 18:02:40 +0100

    Initial commit

---
+++ android/app/src/main/java/com/eshop/vendor/vendor_eshop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.urc.seller;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,18 @@
+final String appName = 'URC Seller';
+
+final String packageName = 'com.urc.seller';
+
+final String iosPackage = 'com.urc.seller';
+// https://alphawizztest.tk/Food_on_the_way/app/v1/api/
+// final String baseUrl = 'https://foodontheways.com/seller/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/URC/seller/app/v1/api/';
+final String jwtKey = "f78d95e500c7ef84ee99d69d9651ff7a229979a2";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
